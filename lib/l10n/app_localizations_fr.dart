2cdb4954e4af71a8e2196bc5486a61215dcf17f1
amine.elalame <elm.dev.code@gmail.com>
2025-05-29 17:57:22 +0200

    feat: setup internationalization (i18n) with English and French

---
+++ lib/l10n/app_localizations_fr.dart
@@ -0,0 +1,17 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for French (`fr`).
+class AppLocalizationsFr extends AppLocalizations {
+  AppLocalizationsFr([String locale = 'fr']) : super(locale);
+
+  @override
+  String get landingPageTitle => 'Découvrez & collectionnez des œuvres d\'art';
+
+  @override
+  String get landingPageSubtitle =>
+      'Scannez les tags NFC dans les musées pour découvrir des chefs-d\'œuvre 🔍';
+}
