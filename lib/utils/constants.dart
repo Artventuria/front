499fdf0b671b87290736cac8f61e61fc1356a09b
amine.elalame <elm.dev.code@gmail.com>
2025-06-10 19:52:56 +0200

    style(constants): format color constant for better readability

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -12,14 +12,14 @@ class AppColors {
   static const transparent = Colors.transparent;
   static const solidPinkBackground = Color(0xFFF1CBD1);
   static const errorColor = Color(0xFFE57373);
-  
+
   // Home page colors
   static const homeBackgroundEnd = Color(0xFFFCF3F4);
   static const subtitlePink = Color(0xFFD2BBC7);
   static const searchIconColor = Color(0xFFDABCCB);
   static const searchIcon = Color(0xFFDABCCB);
   static const textDarkBrown = Color(0xFF56494E);
-  
+
   // Artwork item colors
   static const textGrey = Color(0xFF888888);
   static const textLightGrey = Color(0xFFAAAAAA);
@@ -32,7 +32,7 @@ class AppColors {
   // Circular artwork gradient colors
   static const circularGradientStart = Color(0xFFFFA176);
   static const circularGradientEnd = Color(0xFF888DFA);
-  
+
   // Leaderboard colors
   static const buttonPurple = Color(0xFF372940);
   static const searchGradientStart = Color(0xFFFFAE88);
@@ -42,12 +42,13 @@ class AppColors {
   static const medalSilver = Color(0xFFC0C0C0);
   static const medalBronze = Color(0xFFCD7F32);
   static const avatarShadow = Color(0xC7F6D1EB);
-  
+
   // My Collection page colors
   static const statCardBackground = Color(0xFFFAE7E9);
   static const dividerGrey = Color(0x33808080); // Colors.grey with alpha 0.2
   static const imageErrorBackground = Color(0xFFD3D3D3); // Colors.grey[300]
-  static const transparentBlack = Color(0x80000000); // Colors.black with alpha 80
+  static const transparentBlack =
+      Color(0x80000000); // Colors.black with alpha 80
 }
 
 class AppTextStyles {
